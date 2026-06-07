1bc6424e21c1559a3812020bf4ed4630d355032a
MarkCMO <mark@markcmo.com>
2026-06-07 17:03:03 -0400

    Add film-search route wrapper

---
+++ functions/film-search/[[path]].js
@@ -0,0 +1,4 @@
+// AUTO-GENERATED. Do not edit.
+import { dispatchSingle } from '../_lib/netlify-shim.js';
+import * as mod from '../../netlify/functions/film-search.js';
+export async function onRequest(context) { return dispatchSingle(mod, context); }
