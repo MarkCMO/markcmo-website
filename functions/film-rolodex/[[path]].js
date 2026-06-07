7f0f4e4b7f4aee4d81072f8a2d909ab989659ebd
MarkCMO <mark@markcmo.com>
2026-06-07 16:12:58 -0400

    cache-bust film-rolodex to rebundle with shim env fallback

--- functions/film-rolodex/[[path]].js
+++ functions/film-rolodex/[[path]].js
@@ -1,5 +1,5 @@
 // AUTO-GENERATED. Do not edit.
-// cache-bust 2026-06-07: force CF Pages rebundle after blobs setJSON fix
+// cache-bust 2026-06-07c: rebundle with MARKCMO_SUPABASE fallback in shim
 import { dispatchSingle } from '../_lib/netlify-shim.js';
 import * as mod from '../../netlify/functions/film-rolodex.js';
 export async function onRequest(context) { return dispatchSingle(mod, context); }
