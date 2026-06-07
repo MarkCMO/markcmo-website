006e92837ca589ada826d40f87ebb1c0c8ea0f69
MarkCMO <mark@markcmo.com>
2026-06-07 16:01:40 -0400

    cache-bust film-rolodex wrapper to force CF rebundle (fixes setJSON)

--- functions/film-rolodex/[[path]].js
+++ functions/film-rolodex/[[path]].js
@@ -1,4 +1,5 @@
 // AUTO-GENERATED. Do not edit.
+// cache-bust 2026-06-07: force CF Pages rebundle after blobs setJSON fix
 import { dispatchSingle } from '../_lib/netlify-shim.js';
 import * as mod from '../../netlify/functions/film-rolodex.js';
 export async function onRequest(context) { return dispatchSingle(mod, context); }
