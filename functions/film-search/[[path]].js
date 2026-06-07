57dcac6175387286406bb8f89c58218a2b3855a0
MarkCMO <mark@markcmo.com>
2026-06-07 17:09:56 -0400

    register film-search route (cache-bust)

--- functions/film-search/[[path]].js
+++ functions/film-search/[[path]].js
@@ -1,4 +1,5 @@
 // AUTO-GENERATED. Do not edit.
+// v2 register route
 import { dispatchSingle } from '../_lib/netlify-shim.js';
 import * as mod from '../../netlify/functions/film-search.js';
 export async function onRequest(context) { return dispatchSingle(mod, context); }
